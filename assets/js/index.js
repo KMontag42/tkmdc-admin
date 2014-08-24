fcf9508ca8fd63769e09d71b08939e9e975839d6
Kyle Montag <kmontag@skillz.com>
2014-08-24 00:33:41 -0400

    show alert that user isn't allowed

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,16 +6,20 @@
  *********************************************************************************************************************************/
 (function($) {
 
+    $(".alert").alert().hide();
+
     var myRef = new Firebase("https://tkmdc.firebaseio.com"),
         authClient = new FirebaseSimpleLogin(myRef, function(error, user) {
         if (error) {
             // an error occurred while attempting login
-            console.log(error);
+            $("#login-error-alert").show();
         } else if (user) {
             // user authenticated with Firebase
             console.log("User ID: " + user.uid + ", Provider: " + user.provider);
             if (user.uid === "github:1686738") {
                 window.location = "dashboard.html"
+            } else {
+                $("#login-warning-alert").show();
             }
         } else {
             // user is logged out
